a32037b84991c2bc5fee56de417866ad3833255c
mkasabov98 <martinkasabov98@gmail.com>
2023-10-16 12:45:21 +0300

    added login logic, created user bluePrint

---
+++ human-capital-management/src/app/data/users.ts
@@ -0,0 +1,92 @@
+export const USERS = [
+    {
+      "email": "shawnagrant@assistia.com",
+      "password": "brown12345",
+      "firstName": "Sherri",
+      "lastName": "Robles"
+    },
+    {
+      "email": "sherrirobles@assistia.com",
+      "password": "blue12345",
+      "firstName": "Elaine",
+      "lastName": "Maddox"
+    },
+    {
+      "email": "elainemaddox@assistia.com",
+      "password": "green12345",
+      "firstName": "Gallegos",
+      "lastName": "Browning"
+    },
+    {
+      "email": "gallegosbrowning@assistia.com",
+      "password": "blue12345",
+      "firstName": "Hatfield",
+      "lastName": "Hurley"
+    },
+    {
+      "email": "hatfieldhurley@assistia.com",
+      "password": "blue12345",
+      "firstName": "Dominique",
+      "lastName": "Castro"
+    },
+    {
+      "email": "dominiquecastro@assistia.com",
+      "password": "green12345",
+      "firstName": "Wagner",
+      "lastName": "Morrow"
+    },
+    {
+      "email": "wagnermorrow@assistia.com",
+      "password": "green12345",
+      "firstName": "Fields",
+      "lastName": "Colon"
+    },
+    {
+      "email": "fieldscolon@assistia.com",
+      "password": "green12345",
+      "firstName": "Estes",
+      "lastName": "Marsh"
+    },
+    {
+      "email": "estesmarsh@assistia.com",
+      "password": "blue12345",
+      "firstName": "Jill",
+      "lastName": "Harvey"
+    },
+    {
+      "email": "jillharvey@assistia.com",
+      "password": "brown12345",
+      "firstName": "Garner",
+      "lastName": "Frye"
+    },
+    {
+      "email": "garnerfrye@assistia.com",
+      "password": "blue12345",
+      "firstName": "Herman",
+      "lastName": "Anderson"
+    },
+    {
+      "email": "hermananderson@assistia.com",
+      "password": "green12345",
+      "firstName": "Lindsay",
+      "lastName": "Randall"
+    },
+    {
+      "email": "lindsayrandall@assistia.com",
+      "password": "green12345",
+      "firstName": "Luz",
+      "lastName": "Massey"
+    },
+    {
+      "email": "luzmassey@assistia.com",
+      "password": "green12345",
+      "firstName": "English",
+      "lastName": "Horne"
+    },
+    {
+      "email": "englishhorne@assistia.com",
+      "password": "blue12345",
+      "firstName": "Fisher",
+      "lastName": "Bates"
+    }
+  ]
