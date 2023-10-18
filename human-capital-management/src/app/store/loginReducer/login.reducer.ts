358d01ca1a5fd97778c4a36a5b97d9a53053839d
mkasabov98 <martinkasabov98@gmail.com>
2023-10-18 23:09:34 +0300

    updated the user bluePrint, the login page, added reducer for the users collection and action for creating a new user

--- human-capital-management/src/app/store/loginReducer/login.reducer.ts
+++ human-capital-management/src/app/store/loginReducer/login.reducer.ts
@@ -4,12 +4,13 @@ import { StoreInterface } from "../store.interface";
 import { userInterface } from "src/app/interfaces/user.interface";
 
 
-type UserState = null | userInterface;
+
+type UserState = null | number;
 
 const initialState: UserState = null;
 
 export const loginReducer = createReducer<UserState>(
     initialState,
-    on(login, (state, user: userInterface) => user),
+    on(login, (state, user: userInterface) => user.id),
     on(logout, (state) => null)
 )
