238d0b363bde93580b5851b506db8ec33ce14ffe
mkasabov98 <martinkasabov98@gmail.com>
2023-10-24 00:57:19 +0300

    Finished the functionality for changing the password, photo, email and phone number in the profile page

--- human-capital-management/src/app/store/userCollectionReducer/user-collection.actions.ts
+++ human-capital-management/src/app/store/userCollectionReducer/user-collection.actions.ts
@@ -8,6 +8,21 @@ export const addUser = createAction(
 )
 
 export const changePassword = createAction(
-    "[Change Password]",
+    "[Change Password] ChangePassword",
     props<{userID:number, newPassword:string}>()
+)
+
+export const changeEmail = createAction(
+    "[Change Email] ChangeEmail",
+    props<{userID:number, newEmail:string}>()
+)
+
+export const chnagePhoneNumber = createAction(
+    "[Change Phone Number] ChangePhoneNumber",
+    props<{userID:number, newPhoneNumber:number}>()
+)
+
+export const changeProfilePhoto = createAction(
+    "[Change Profile Photo] ChangeProfilePhoto",
+    props<{userID:number, newProfilePhoto: string }>()
 )
--- human-capital-management/src/app/store/userCollectionReducer/user-collection.reducer.ts
+++ human-capital-management/src/app/store/userCollectionReducer/user-collection.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from "@ngrx/store";
 import { userInterface } from "src/app/interfaces/user.interface";
-import { addUser, changePassword } from "./user-collection.actions";
+import { addUser, changeEmail, changePassword, changeProfilePhoto, chnagePhoneNumber } from "./user-collection.actions";
 import { USERS } from "src/app/data/users";
 
 const initialState: userInterface[] = USERS;
@@ -10,9 +10,9 @@ export const userCollectionReducer = createReducer(
     on(addUser, (state, user: userInterface) => {
         return [...state, user]
     }),
-    on(changePassword, (state, {userID, newPassword}) => {
+    on(changePassword, (state, { userID, newPassword }) => {
         const userToUpdateIndex = state.findIndex(user => user.id === userID);
-    
+
         if (userToUpdateIndex !== -1) {
             const userToUpdate = { ...state[userToUpdateIndex], password: newPassword };
             return [
@@ -22,6 +22,44 @@ export const userCollectionReducer = createReducer(
             ];
         }
         return state;
+    }),
+    on(changeEmail, (state, { userID, newEmail }) => {
+        const userToUpdateIndex = state.findIndex(user => user.id === userID);
+
+        if (userToUpdateIndex !== -1) {
+            const userToUpdate = { ...state[userToUpdateIndex], email: newEmail };
+            return [
+                ...state.slice(0, userToUpdateIndex),
+                userToUpdate,
+                ...state.slice(userToUpdateIndex + 1),
+            ];
+        }
+        return state;
+    }),
+    on(chnagePhoneNumber, (state, {userID, newPhoneNumber}) => {
+        const userToUpdateIndex = state.findIndex(user => user.id === userID);
+
+        if (userToUpdateIndex !== -1) {
+            const userToUpdate = { ...state[userToUpdateIndex], phoneNumber: newPhoneNumber };
+            return [
+                ...state.slice(0, userToUpdateIndex),
+                userToUpdate,
+                ...state.slice(userToUpdateIndex + 1),
+            ];
+        }
+        return state;
+    }),
+    on(changeProfilePhoto, (state, {userID, newProfilePhoto}) => {
+        const userToUpdateIndex = state.findIndex(user => user.id === userID);
+
+        if (userToUpdateIndex !== -1) {
+            const userToUpdate = { ...state[userToUpdateIndex], profilePhoto: newProfilePhoto };
+            return [
+                ...state.slice(0, userToUpdateIndex),
+                userToUpdate,
+                ...state.slice(userToUpdateIndex + 1),
+            ];
+        }
+        return state;
     })
-    
 )
