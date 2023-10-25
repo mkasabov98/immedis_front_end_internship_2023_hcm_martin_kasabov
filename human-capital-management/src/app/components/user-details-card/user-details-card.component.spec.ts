bcabf370ec4d60cda6c1a966de4166442655091e
mkasabov98 <martinkasabov98@gmail.com>
2023-10-26 02:58:16 +0300

    The page for creating a user is completed, styled as well. Started working on the home page, main logic is done, visualisation of the 2 containers holding users and styling is left.

---
+++ human-capital-management/src/app/components/user-details-card/user-details-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserDetailsCardComponent } from './user-details-card.component';
+
+describe('UserDetailsCardComponent', () => {
+  let component: UserDetailsCardComponent;
+  let fixture: ComponentFixture<UserDetailsCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserDetailsCardComponent]
+    });
+    fixture = TestBed.createComponent(UserDetailsCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
